d3fbd99fd57448eeee5fdbc6732e725e4c0c515d
Jake Pitkin <jakepitkin@gmail.com>
2018-06-13 10:08:41 -0600

    KDTree working

--- hw4/KDTree.hpp
+++ hw4/KDTree.hpp
@@ -9,32 +9,122 @@ template<int Dimension>
 class KDTree{
 public:
 
-  KDTree(const std::vector<Point<Dimension> >& points){
-  }
+    KDTree(const std::vector<Point<Dimension> >& points){
+        std::vector<Point<Dimension>> myCopy = points;
+        // All of the points belong to the root
+        root = std::unique_ptr<Node<0>>(new Node<0>(myCopy.begin(), myCopy.end()));
+    }
+
+    std::vector<Point<Dimension>> rangeQuery(const Point<Dimension>& p, float radius) const {
+        std::vector<Point<Dimension>> ret;
+        rangeQueryRecurse(root, p, radius, ret);
+
+        return ret;
+    }
+
+    std::vector<Point<Dimension>> KNN(const Point<Dimension>& p, int k) const {
+        std::vector<Point<Dimension>> pQueue;
+        AABB<Dimension> aabb;
+        knnRecurse(root, p, k, aabb, pQueue);
+        
+        return pQueue;
+    }
   
 private:
   
-  template<int SplitDimension> //Don't store the split dimension explicitly
-  struct Node{
-	Point<Dimension> p;
-	//The children will have the "next" splitting dimension
-	//Since this is part of the type, we can't "forget" to set it properly... nice!
-	std::unique_ptr<Node< (SplitDimension + 1)%Dimension> >  left, right;
+    template<int SplitDimension> //Don't store the split dimension explicitly
+    struct Node{
+        Point<Dimension> p;
+        //The children will have the "next" splitting dimension
+        //Since this is part of the type, we can't "forget" to set it properly... nice!
+        std::unique_ptr<Node< (SplitDimension + 1)%Dimension> >  left, right;
 
 
-	/*
-	  We'll use iterators to describe the chunk of our points array that belong to this node/subtree
-	 */
-	template<typename Iter>
-	Node(Iter begin, Iter end)
-	{
-	  //Our children (if we have any) will use the "next" splitting dimension
-	  using ChildType = Node<(SplitDimension +1)%Dimension>;
-	  
-	  
-	}
-	
-  };
+        /*
+          We'll use iterators to describe the chunk of our points array that belong to this node/subtree
+         */
+        template<typename Iter>
+        Node(Iter begin, Iter end)
+        {
+            //Our children (if we have any) will use the "next" splitting dimension
+            using ChildType = Node<(SplitDimension +1)%Dimension>;
+
+            auto middle = begin + (end - begin)/2;
   
+            std::nth_element(begin, middle, end, CompareBy<SplitDimension>{});
+            p = *middle;
+            if(begin != middle){
+                left = std::unique_ptr<ChildType>(new ChildType(begin, middle));
+            }
+            if(middle +1 != end){
+                right = std::unique_ptr<ChildType>(new ChildType(middle + 1, end));
+            }
+        }
+    };
+
+    std::unique_ptr<Node<0>> root;
+  
+    template<int SplitDimension>
+    void rangeQueryRecurse(const std::unique_ptr<Node<SplitDimension> >& node,
+        const Point<Dimension>& p,
+        float radius,
+        std::vector<Point<Dimension> >& ret) const{
+
+      if(distance(p, node->p) < radius){
+        ret.push_back(node->p);
+      }
+
+      if(node->left && node->p[SplitDimension] >= (p[SplitDimension] - radius)){
+        rangeQueryRecurse(node->left, p, radius, ret);
+      }
+
+      if(node->right && node->p[SplitDimension] <= (p[SplitDimension] + radius)){
+        rangeQueryRecurse(node->right, p, radius, ret);
+      }
+      
+
+    }
+
+    template <int SplitDimension>
+    void knnRecurse(const std::unique_ptr<Node<SplitDimension> >& node,
+        const Point<Dimension>& p,
+        int k,
+        const AABB<Dimension>& aabb,
+        std::vector< Point<Dimension> >& pQueue) const{ 
+
+      using ChildType = Node<(SplitDimension +1)%Dimension>;
+      DistanceComparator<Dimension> cmp{p};
+      
+      if(pQueue.size() < k){
+
+        pQueue.push_back(node->p);
+        std::push_heap(pQueue.begin(), pQueue.end(), cmp);
+        
+      } else if (distance(node->p, p) < distance(pQueue.front(), p)){
+
+        std::pop_heap(pQueue.begin(), pQueue.end(), cmp);
+        pQueue.pop_back();
+        pQueue.push_back(node->p);
+        std::push_heap(pQueue.begin(), pQueue.end(), cmp);
+        
+      }
+
+      if(node->left){
+        AABB leftBox = aabb;
+        leftBox.maxs[SplitDimension] = node->p[SplitDimension];
+        if(pQueue.size() < k || distance(leftBox.closestInBox(p), p) < distance(pQueue.front(), p)){
+      	knnRecurse(node->left, p, k, leftBox, pQueue);
+        }
+      }
+
+      if(node->right){
+        AABB rightBox = aabb;
+        rightBox.mins[SplitDimension] = node->p[SplitDimension];
+        if(pQueue.size() < k || distance(rightBox.closestInBox(p), p) < distance(pQueue.front(), p)){
+      	knnRecurse(node->right, p, k, rightBox, pQueue);
+        }
+      }
+      
+    }
 };
 
--- hw4/main.cpp
+++ hw4/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "BucketKNN.hpp"
 #include "Generators.hpp"
+#include "KDTree.hpp"
 #include "Point.hpp"
 
 
@@ -8,10 +9,12 @@ template<int Dimension>
 const std::vector<Point<Dimension>> generate_points(int n_samples);
 float random_float(float min, float max);
 void test_bucketing();
+void test_KDTree();
 
 
 int main(int argc, const char * argv[]) {
-    test_bucketing();
+    //test_bucketing();
+    test_KDTree();
 
     return 0;
 }
@@ -32,6 +35,27 @@ void test_bucketing() {
     BucketKNN bucket = BucketKNN(points, divisions);
     Point<dimension> p = generator.generatePoint();
     std::vector<Point<dimension>> neighbours = bucket.KNN(p, 3);
+    std::cout << "Point: " << p << std::endl;
+    for (Point<dimension> n : neighbours) {
+        std::cout << "Neighbour: " << n << std::endl;
+    }
+}
+
+void test_KDTree() {
+    const int dimension = 2;
+    float min = 0;
+    float max = 99;
+    int n_samples = 1000;
+    int k = 2;
+
+    std::vector<Point<dimension>> points;
+    UniformGenerator generator = UniformGenerator <dimension> (min, max);
+    for (int i = 0; i < n_samples; i++) {
+        points.push_back(generator.generatePoint());
+    }
+    KDTree tree = KDTree(points);
+    Point<dimension> p = generator.generatePoint();
+    std::vector<Point<dimension>> neighbours = tree.KNN(p, k);
     std::cout << "Point: " << p << std::endl;
     for (Point<dimension> n : neighbours) {
         std::cout << "Neighbour: " << n << std::endl;
