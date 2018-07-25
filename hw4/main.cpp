e68f1462c7e7db2dcf3ab1f0c29e240a994ff379
Jake Pitkin <jakepitkin@gmail.com>
2018-07-25 13:36:52 -0600

    update

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -3,70 +3,153 @@
 #include "Generators.hpp"
 #include "KDTree.hpp"
 #include "Point.hpp"
+#include "QuadTree.hpp"
 
 
 void test_bucketing();
-void test_KDTree();
+void test_kdtree();
+void test_quadtree();
 
 
 int main(int argc, const char * argv[]) {
-    //test_bucketing();
-    test_KDTree();
+    test_bucketing();
+    test_kdtree();
+    test_quadtree();
 
     return 0;
 }
 
 void test_bucketing() {
-    const int dimension = 2;
-    float min = 0;
-    float max = 99;
-    int n_samples = 10;
-    int k = 2;
+    const int dimension = 4;
+    float min = -100;
+    float max = 100;
+    int n_samples = 10000;
+    int k = 5;
     int divisions = 10;
+    float radius = 20;
 
     std::vector<Point<dimension>> points;
+    std::vector<Point<dimension>> rq_points;
     UniformGenerator generator = UniformGenerator <dimension> (min, max);
+    Point<dimension> qp = generator.generatePoint();
+    DistanceComparator<dimension> cmp{qp};
     for (int i = 0; i < n_samples; i++) {
-        points.push_back(generator.generatePoint());
+      Point<dimension> p = generator.generatePoint();
+      if (distance(p, qp) <= radius) {
+        rq_points.push_back(p);
+      }
+      points.push_back(p);
     }
     BucketKNN bucket = BucketKNN(points, divisions);
-    Point<dimension> p = generator.generatePoint();
-    std::vector<Point<dimension>> neighbours = bucket.KNN(p, 3);
-    std::cout << "Point: " << p << std::endl;
-    for (Point<dimension> n : neighbours) {
-        std::cout << "Neighbour: " << n << std::endl;
+    std::vector knn = bucket.KNN(qp, k);
+    std::vector range_points = bucket.rangeQuery(qp, radius);
+    std::sort(points.begin(), points.end(), cmp);
+    std::sort(rq_points.begin(), rq_points.end(), cmp);
+    std::sort(range_points.begin(), range_points.end(), cmp);
+    std::sort(knn.begin(), knn.end(), cmp);
+    points.resize(k);
+    for (int i = 0; i < k; i++) {
+      if (distance(points[i], knn[i]) != 0) {
+        std::cout << "bucket knn failed on " << i << std::endl;
+      }
     }
+    std::cout << "bucket knn passed" << std::endl << std::endl;
+
+    for (int i = 0; i < rq_points.size(); i++) {
+      if (distance(range_points[i], rq_points[i]) != 0) {
+        std::cout << "bucket rq failed on " << i << std::endl;
+      }
+    }
+    std::cout << "bucket rq passed" << std::endl << std::endl;
+  
 }
 
-void test_KDTree() {
-    const int dimension = 1;
-    int *dim_pointer = (int *) (&dimension);
-    float min = 0;
-    float max = 99;
-    int n_samples = 3;
-    int k = 2;
+void test_kdtree() {
+    const int dimension = 3;
+    float min = -100;
+    float max = 100;
+    int n_samples = 10000;
+    int k = 5;
+    float radius = 20;
 
-    for (int j = 2; j < 5; j++) {
-        std::cout << "Variable: " << dimension << std::endl;
-        std::vector<Point<dimension>> points;
-        UniformGenerator<dimension> generator = UniformGenerator <dimension> (min, max);
-        KDTree tree = KDTree(points);
-        *dim_pointer = j;
-    }
-    return;
-
-    std::vector<Point<1>> points;
-    UniformGenerator<1> generator = UniformGenerator <1> (min, max);
+    std::vector<Point<dimension>> points;
+    std::vector<Point<dimension>> rq_points;
+    UniformGenerator generator = UniformGenerator <dimension> (min, max);
+    Point<dimension> qp = generator.generatePoint();
+    DistanceComparator<dimension> cmp{qp};
     for (int i = 0; i < n_samples; i++) {
-        Point<dimension> p = generator.generatePoint();
-        points.push_back(p);
+      Point<dimension> p = generator.generatePoint();
+      if (distance(p, qp) <= radius) {
+        rq_points.push_back(p);
+      }
+      points.push_back(p);
     }
-    KDTree<1> tree = KDTree<1>(points);
-    Point<dimension> p = generator.generatePoint();
-    std::cout << "Point: " << p << std::endl;
-    //tree.rangeQuery(p, 1);
-    std::vector<Point<dimension>> neighbours = tree.KNN(p, 1);
-    //for (Point<dimension> n : neighbours) {
-    //    std::cout << "Neighbour: " << n << std::endl;
-    //}
+    KDTree tree = KDTree(points);
+    std::vector knn = tree.KNN(qp, k);
+    std::vector range_points = tree.rangeQuery(qp, radius);
+    std::sort(points.begin(), points.end(), cmp);
+    std::sort(rq_points.begin(), rq_points.end(), cmp);
+    std::sort(range_points.begin(), range_points.end(), cmp);
+    std::sort(knn.begin(), knn.end(), cmp);
+    points.resize(k);
+    for (int i = 0; i < k; i++) {
+      if (distance(points[i], knn[i]) != 0) {
+        std::cout << "kdtree knn failed on " << i << std::endl;
+      }
+    }
+    std::cout << "kdtree knn passed" << std::endl << std::endl;
+
+    for (int i = 0; i < rq_points.size(); i++) {
+      if (distance(range_points[i], rq_points[i]) != 0) {
+        std::cout << "kdtree rq failed on " << i << std::endl;
+      }
+    }
+    std::cout << "kdtree rq passed" << std::endl << std::endl;
+  
 }
+
+void test_quadtree() {
+    const int dimension = 2;
+    float min = -100;
+    float max = 100;
+    int n_samples = 10000;
+    int k = 5;
+    int max_size = 10;
+    float radius = 3;
+
+    std::vector<Point<dimension>> points;
+    std::vector<Point<dimension>> rq_points;
+    UniformGenerator generator = UniformGenerator <dimension> (min, max);
+    Point<dimension> qp = generator.generatePoint();
+    DistanceComparator<dimension> cmp{qp};
+    for (int i = 0; i < n_samples; i++) {
+      Point<dimension> p = generator.generatePoint();
+      if (distance(p, qp) <= radius) {
+        rq_points.push_back(p);
+      }
+      points.push_back(p);
+    }
+    QuadTree tree = QuadTree(points, max_size);
+    std::vector knn = tree.KNN(qp, k);
+    std::vector range_points = tree.rangeQuery(qp, radius);
+    std::sort(points.begin(), points.end(), cmp);
+    std::sort(rq_points.begin(), rq_points.end(), cmp);
+    std::sort(range_points.begin(), range_points.end(), cmp);
+    std::sort(knn.begin(), knn.end(), cmp);
+    points.resize(k);
+    for (int i = 0; i < k; i++) {
+      if (distance(points[i], knn[i]) != 0) {
+        std::cout << "quadtree knn failed on " << i << std::endl;
+      }
+    }
+    std::cout << "quadtree knn passed" << std::endl << std::endl;
+
+    for (int i = 0; i < rq_points.size(); i++) {
+      if (distance(range_points[i], rq_points[i]) != 0) {
+        std::cout << "quadtree rq failed on " << i << std::endl;
+      }
+    }
+    std::cout << "quadtree rq passed" << std::endl << std::endl;
+  
+}
+
